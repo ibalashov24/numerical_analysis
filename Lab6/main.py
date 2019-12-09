11d5e1ec3fd00059eed4ee76af83e7248387d989
Ilya Balashov <ibalashov24@gmail.com>
2019-12-10 02:01:11 +0300

    Make like-gauss method non-composite

--- Lab6/main.py
+++ Lab6/main.py
@@ -19,72 +19,6 @@ def omega(x, roots, N, j):
 
     return result
 
-def likeGauss(func, weight, N, M, A, B, isDebugInfo=False):
-    if isDebugInfo: print("Like-Gauss integral calculation:")
-
-    integral = .0
-    for i in range(0, M):
-        a = A + (B - A) / M * i
-        b = a + (B - A) / M 
-
-        if isDebugInfo: print("Range from {0} to {1}".format(a, b))
-
-        # Calculating moments
-        if isDebugInfo: print("Moments of the weight function:")
-        moment = []
-        for j in range(0, 2*N):
-            moment.append(integrate.quad(lambda x: weight(x) * pow(x, j), a, b)[0])
-            if isDebugInfo: print("mu_{0} = {1}".format(j, moment[-1]))
-
-        # Solving system of linear equations
-        linalgLeft = np.array([])
-        for j in range(N - 1, 2*N - 1):
-            for k in range(0, N):
-                linalgLeft = np.append(linalgLeft, moment[j - k])
-        linalgLeft = linalgLeft.reshape(N, N)
-
-        linalgRight = np.array([])
-        for j in range(N, 2*N):
-            linalgRight = np.append(linalgRight, [-moment[j]])
-
-        # Orthogonal polynom
-        polynom = np.linalg.solve(linalgLeft, linalgRight)
-        polynom = np.insert(polynom, 0, [1.0])
-
-        if isDebugInfo: print("Orthogonal polynom:")
-        for i in range(0, N):
-            if isDebugInfo: print("{0}x^{1}".format(polynom[i], N - i), end='')
-            if isDebugInfo and polynom[i+1] >= 0:
-                print("+", end='')
-        if isDebugInfo: print(polynom[N])
-
-        # Finding roots of the polynom
-        roots = np.roots(polynom)
-        if isDebugInfo:
-            print("Nodes of quadrature polynom:")
-            print(roots)
-
-        # Finding quadrature polynom coefficients
-        bigA = []
-        for j in range(0, N):
-            bigA.append(integrate.quad(
-                lambda x: weight(x) * omega(x, roots, N, j), a,  b)[0])
-
-        if isDebugInfo:
-            print("Quadrature polynom coefficients:")
-            print(bigA)
-
-        # Calculating integral
-        result = .0
-        for j in range(0, N):
-            result += bigA[j] * func(roots[j])
-
-        integral += result
-
-        if isDebugInfo: print()
-
-    return integral
-
 def legendre(x, N):
     """
     Legendre polynom
@@ -123,6 +57,77 @@ def gauss(func, N, M, A, B):
 
     return integral
 
+def likeGauss(func, weight, N, M, A, B, isDebugInfo=False):
+    if isDebugInfo: print("Like-Gauss integral calculation:")
+
+    integral = .0
+#    for i in range(0, M):
+#        a = A + (B - A) / M * i
+#        b = a + (B - A) / M 
+
+    a = A
+    b = B
+
+    if isDebugInfo: print("Range from {0} to {1}".format(a, b))
+
+    # Calculating moments
+    if isDebugInfo: print("Moments of the weight function:")
+    moment = []
+    for j in range(0, 2*N):
+        moment.append(integrate.quad(lambda x: weight(x) * pow(x, j), a, b)[0])
+#            moment.append(gauss(lambda x: weight(x) * pow(x, j), N, M, a, b))
+        if isDebugInfo: print("mu_{0} = {1}".format(j, moment[-1]))
+
+    # Solving system of linear equations
+    linalgLeft = np.array([])
+    for j in range(N - 1, 2*N - 1):
+        for k in range(0, N):
+            linalgLeft = np.append(linalgLeft, moment[j - k])
+    linalgLeft = linalgLeft.reshape(N, N)
+
+    linalgRight = np.array([])
+    for j in range(N, 2*N):
+        linalgRight = np.append(linalgRight, [-moment[j]])
+
+    # Orthogonal polynom
+    polynom = np.linalg.solve(linalgLeft, linalgRight)
+    polynom = np.insert(polynom, 0, [1.0])
+
+    if isDebugInfo: print("Orthogonal polynom:")
+    for i in range(0, N):
+        if isDebugInfo: print("{0}x^{1}".format(polynom[i], N - i), end='')
+        if isDebugInfo and polynom[i+1] >= 0:
+            print("+", end='')
+    if isDebugInfo: print(polynom[N])
+
+    # Finding roots of the polynom
+    roots = np.roots(polynom)
+    if isDebugInfo:
+        print("Nodes of quadrature polynom:")
+        print(roots)
+
+    # Finding quadrature polynom coefficients
+    bigA = []
+    for j in range(0, N):
+        bigA.append(integrate.quad(
+            lambda x: weight(x) * omega(x, roots, N, j), a,  b)[0])
+
+    if isDebugInfo:
+        print("Quadrature polynom coefficients:")
+        print(bigA)
+
+    # Calculating integral
+    result = .0
+    for j in range(0, N):
+        result += bigA[j] * func(roots[j])
+
+    integral += result
+
+    if isDebugInfo: print()
+
+    return integral
+
+
 def meler(func, N):
     result = .0
 
@@ -144,6 +149,6 @@ B = float(input())
 
 print()
 print("3) Интеграл методом Гаусса: ", gauss(lambda x: f(x) * w(x), N, M, A, B)) 
-print("4) Интеграл методом типа Гаусса: ", likeGauss(f, w, N, M, A, B))
+print("4) Интеграл методом типа Гаусса: ", likeGauss(f, w, N, M, A, B, True))
 print("5) Интеграл по формуле Мелера: ", meler(f, N))
 
