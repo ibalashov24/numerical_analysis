7bf00d361b1c0792b8c1de53fedf5b3cb2289834
Ilya Balashov <ibalashov24@gmail.com>
2020-02-18 00:57:30 +0300

    Implement linear system solution finder

--- semester_6/Lab1/Program.cs
+++ semester_6/Lab1/Program.cs
@@ -7,9 +7,12 @@ namespace Lab1
     using Matrix = List<List<double>>;
     using Row = List<double>;
     using Column = List<double>;
+        
 
     class Program
-    {
+    {  
+        static private readonly double Epsilon = 1e-6;
+
         static Matrix ReadMatrix(int height, int width)
         {
             var matrix = new Matrix(height);
@@ -37,6 +40,10 @@ namespace Lab1
             for (int k = 0; k < matrix.Count; ++k)
             {
                 var temp = triangularMatrix[k][k];
+                if (Math.Abs(temp) < Epsilon)
+                {       
+                    Console.WriteLine($"Too small main element a_{k},{k} = {temp}!");
+                }
 
                 for (int j = k + 1; j < matrix.Count + 1; ++j)
                 {
@@ -70,18 +77,137 @@ namespace Lab1
 
         static Row SolveSystemLU(Matrix matrix)
         {
+            var l = new Matrix(matrix.Count);
+            var u = new Matrix(matrix.Count);
 
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                l[i] = new Row(matrix.Count);
+                u[i] = new Row(matrix.Count);
+            }
+
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                for (int j = i; j < matrix.Count; ++j)
+                {
+                    l[j][i] = matrix[j][i];
+                    for (int k = 0; k < i; ++k)
+                    {
+                        l[j][i] -= l[j][k] * u[k][i];
+                    }
+
+                    u[i][j] = matrix[i][j];
+                    for (int k = 0; k < i; ++k)
+                    {
+                        u[i][j] -= l[i][k] * u[k][j];
+                    }
+                    u[i][j] /= l[i][i];
+                } 
+            }
+
+            // Solving Ly = b
+            var middleSolution = new Row(matrix.Count);
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                middleSolution[i] = matrix[i][matrix.Count];
+                for (int j = 0; j < i; ++j)
+                {
+                    middleSolution[i] -= matrix[i][j] * middleSolution[j];
+                }
+                middleSolution[i] /= matrix[i][i];
+            }
+
+            // Solving Ux = y
+            var solution = new Row(matrix.Count);
+            for (int i = matrix.Count - 1; i >= 0; ++i)
+            {
+                solution[i] = matrix[i][matrix.Count];
+                for (int j = i + 1; j < matrix.Count; ++j)
+                {
+                    solution[i] -= matrix[i][j] * solution[j];
+                }
+            }
+            
+            return solution;
         }
 
         static Row SolveSystemMainElement(Matrix matrix)
         {
+            Matrix triangularMatrix = new Matrix(matrix.Count);
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                triangularMatrix[i] = new Column(matrix.Count + 1);
+            }
+            
+            // Forward movement
+            for (int k = 0; k < matrix.Count; ++k)
+            {
+                var largestMainElementLine = k;
+                // Choosing the largest main element
+                for (int i = k + 1; i < matrix.Count; ++i)
+                {   
+                    if (Math.Abs(matrix[i][k]) >= Math.Abs(matrix[largestMainElementLine][k]))
+                    {
+                        largestMainElementLine = i;
+                    }
+                }
+                
+                // Swapping lines
+                var t = matrix[k];
+                matrix[k] = matrix[largestMainElementLine];
+                matrix[largestMainElementLine] = t;
+                
+                // Performing further forward movement
+                var temp = triangularMatrix[k][k];
+                for (int j = k + 1; j < matrix.Count + 1; ++j)
+                {
+                     triangularMatrix[k][j] /= temp;
+                } 
 
+                for (int i = k + 1; i < matrix.Count; ++i)
+                {
+                    temp = triangularMatrix[i][k];
+
+                    for (int j = k + 1; j < matrix.Count + 1; ++j)
+                    {
+                        triangularMatrix[i][j] -= triangularMatrix[k][j] * temp;
+                    }   
+                }
+            }
+
+            // Backward movement
+            var solution = new Row(matrix.Count);
+            for (int i = matrix.Count - 1; i >= 0; --i)
+            {
+                solution[i] = triangularMatrix[i][matrix.Count];
+                for (int j = i + 1; j < matrix.Count; ++j)
+                {
+                    solution[i] -= triangularMatrix[i][j] * solution[j];
+                }
+            }
+
+            return solution;
         }
 
         static Row CalcError(Matrix matrix, Row solution)
         {
+            var estimatedFreeRow = new Row(matrix.Count);
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                estimatedFreeRow[i] = 0;
+                for (int j = 0; j < matrix.Count; ++j)
+                {
+                    estimatedFreeRow[i] += matrix[i][j] * solution[j];
+                }
+            }
 
+            var error = new Row(matrix.Count);
+            for (int i = 0; i < matrix.Count; ++i)
+            {
+                error[i] = matrix[i][matrix.Count] - solution[i];
+            }
 
+            return error;
         }
 
         static void PrintColumn(Column column)
